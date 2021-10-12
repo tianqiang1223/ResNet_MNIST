c8c47ce60622109b57b524011ea877c246f176ed
tianqiang1223 <65889782+tianqiang1223@users.noreply.github.com>
2021-10-12 21:46:23 +0800

    select_device.py

---
+++ select_device.py
@@ -0,0 +1,35 @@
+# 参考YOLOV5
+
+import datetime
+import os
+import torch
+from pathlib import Path
+
+def data_time(path=__file__):
+    t = datetime.datetime.fromtimestamp(Path(path).stat().st_mtime)
+    return f'{t.year}-{t.month}-{t.day}'
+
+def select_device(device='', batch_size=None):
+    s = f'MNIST {data_time()} torch {torch.__version__}'
+    device = str(device).strip().lower().replace('cuda', '')
+    cpu = device == 'cpu'
+    if cpu:
+        os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+    elif device:
+        os.environ['CUDA_VISIBLE_DEVICES'] = device
+        assert torch.cuda.is_available(), f'CUDA unavailable'
+
+    cuda = not cpu and torch.cuda.is_available()
+    if cuda:
+        devices = device.split(',') if device else '0'
+        n = len(devices)
+        if n > 1 and batch_size:
+            assert batch_size % n ==0
+        space = ' ' * (len(s) + 1)
+        for i, d in enumerate(devices):
+            p = torch.cuda.get_device_properties(i)
+            s += f"{'' if i==0 else space } CUDA:{d} ({p.name}, {p.total_memory / 1024 **2}MB)\n"
+    else:
+        s += 'CPU\n'
+    print(s)
+    return torch.device('cuda:0' if cuda else 'cpu')
